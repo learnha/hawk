50cf5b118b1a66be7ea776ae2a3d2d803dc5a187
Kristoffer Grönlund <krig@koru.se>
2017-09-13 14:03:47 +0200

    Dev: Token based authentication outline

--- hawk/app/controllers/api/api_controller.rb
+++ hawk/app/controllers/api/api_controller.rb
@@ -3,8 +3,35 @@
 
 module Api
   class ApiController < ActionController::API
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+
+    before_action :authenticate, except: [ :login ]
+
     def login
-      render json: { "resources": "111" }
+      # authenticate username + password
+      # generate token with expiry
+      render json: { "login": "ok" }
+    end
+
+    protected
+
+    # Authenticate the user with token based authentication
+    def authenticate
+      # authenticate user
+      authenticate_token || render_unauthorized
+    end
+
+    def authenticate_token
+      authenticate_with_http_token do |token, options|
+        # decrypt token
+        # extract current_user + expiry from token
+        @current_user = "hacluster" if token == "1"
+      end
+    end
+
+    def render_unauthorized(realm = "Application")
+      self.headers["WWW-Authenticate"] = %(Token realm="#{realm.gsub(/"/, "")}")
+      render json: 'Bad credentials', status: :unauthorized
     end
   end
 end
