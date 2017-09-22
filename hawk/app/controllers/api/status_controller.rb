89685b4bf8900437368bb88477185bff0108b225
Kristoffer Grönlund <krig@koru.se>
2017-09-22 09:23:35 +0200

    Dev: Simplify routing table for /status

--- hawk/app/controllers/api/status_controller.rb
+++ hawk/app/controllers/api/status_controller.rb
@@ -9,50 +9,6 @@ module Api
       render json: @status.root.to_json
     end
 
-    def cluster
-      render json: @status.cluster.to_json
-    end
-
-    def cluster_state
-      render json: @status.state
-    end
-
-    def cluster_connections
-      render json: @status.connections
-    end
-
-    def cluster_events
-      render json: @status.events
-    end
-
-    def tasks
-      render json: @status.tasks
-    end
-
-    def nodes
-      render json: @status.nodes
-    end
-
-    def resources
-      render json: @status.resources
-    end
-
-    def node
-      render json: @status.node(params[:id])
-    end
-
-    def resource
-      render json: @status.resource(params[:id])
-    end
-
-    def tickets
-      render json: @status.tickets
-    end
-
-    def ticket
-      render json: @status.ticket(params[:id])
-    end
-
     private
 
     def get_status
--- hawk/config/routes.rb
+++ hawk/config/routes.rb
@@ -4,20 +4,6 @@
 Rails.application.routes.draw do
   scope module: 'api', as: 'api', defaults: {format: :json} do
     post '/login', to: "api#login"
-    scope :status do
-      get '/', to: "status#index", as: :status
-      get '/cluster', to: "status#cluster"
-      get '/cluster/state', to: "status#cluster_state"
-      get '/cluster/connections', to: "status#cluster_connections"
-      get '/cluster/events', to: "status#cluster_events"
-      get '/tasks', to: "status#tasks"
-      get '/nodes', to: "status#nodes"
-      get '/resources', to: "status#resources"
-      get '/nodes/:id', to: "status#node"
-      get '/resources', to: "status#resources"
-      get '/resources/:id', to: "status#resource"
-      get '/tickets', to: "status#tickets"
-      get '/tickets/:id', to: "status#ticket"
-    end
+    get '/status', to: "status#index", as: :status
   end
 end
